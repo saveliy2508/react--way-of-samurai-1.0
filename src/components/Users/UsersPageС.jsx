f70f9ab2372280d721efca8eb7cec090b24c221d
Saveliy Volskiy <svolskey2929@gmail.com>
2022-01-20 14:05:29 +0300

    added componentDidMount in UsersPage

--- src/components/Users/UsersPageС.jsx
+++ src/components/Users/UsersPageС.jsx
@@ -4,12 +4,23 @@ import * as axios from 'axios'
 import userPhoto from './../../assets/userPhoto.PNG'
 
 class UsersPage extends React.Component {
-    constructor(props) {
-        super(props);
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            this.props.setUsers(response.data.items)
-        });
+    // constructor(props) {
+    //     super(props);
+    //     if (this.props.users.length === 0) {
+    //         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    //             this.props.setUsers(response.data.items)
+    //         })
+    //     };
+    // }
+
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                this.props.setUsers(response.data.items)
+            })
+        }
     }
+
     render() {
         return (
             <div className={s.wrapper} >
